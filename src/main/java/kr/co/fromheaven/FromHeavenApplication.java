b75e095284114c8bef4d18b7a279599b1ddcd568
SeungjaeKim <kimseungjae@gmail.com>
2021-05-14 23:59:32 +0900

    Initial commit

---
+++ src/main/java/kr/co/fromheaven/FromHeavenApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fromheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromHeavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FromHeavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/fromheaven/FromHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.fromheaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromHeavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
